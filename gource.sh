4cb3cfe8b9961888fe52f94f92e1baa04317b016
Anthony Martin <github@martin-studio.com>
2017-10-12 19:36:08 -0700

    gource

--- gource.sh
+++ gource.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-gource ./ --user-image-dir ~/Desktop --hide usernames -s 0.5 -b 000000 \
+gource ./ --user-image-dir images --hide usernames -s 0.5 -b 000000 \
+  --start-date '2017-05-27 17:23' \
   -1280x720 --output-ppm-stream - |\
   ffmpeg -y -r 28 -f image2pipe -vcodec ppm -i - -vcodec libx264 -preset slow \
   -crf 28 -threads 0 output.mp4
+
